a127177e2f210aab7a0ded01a26413466dcb418a
Med Aziz CHETOUI <chetoui.mohamedaziz@outlook.fr>
2022-06-08 09:56:38 +0200

    - Installation de la dépendance FlexLayout @13.0.0-beta.38 - Ajout des modules à utliser dans l'implémentation des interfaces (Material modules)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+    FlexLayoutModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/config/api.url.config.ts
@@ -0,0 +1,5 @@
+import { environment } from 'src/environments/environment';
+
+export const API_URLS = {
+    OPERATIONs_URL: environment.api_url + 'operations',
+};
