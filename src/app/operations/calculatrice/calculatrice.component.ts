174b7ad4eb7a70d11a905fb2e6e5be5bec9c44f7
Med Aziz CHETOUI <chetoui.mohamedaziz@outlook.fr>
2022-06-08 10:32:03 +0200

    Implémentation des actions de la calculatrice (+, -, *, /, =, C)

--- src/app/operations/calculatrice/calculatrice.component.ts
+++ src/app/operations/calculatrice/calculatrice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OperationsService } from '../operations.service';
 
 @Component({
   selector: 'app-calculatrice',
@@ -7,11 +8,103 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatriceComponent implements OnInit {
 
+  operator!: string;
+  numbers: string = "";
+
+  value1!: any;
+  value2!: any;
+
   operations: string = "0";
 
-  constructor() { }
+  constructor(private service: OperationsService) { }
 
   ngOnInit(): void {
+  }
+
+  selectedNumber(number: string) {
+    if (this.operations === "0") {
+      this.operations = "";
+    }
+    this.numbers += number;
+    this.operations += number;
+  }
+
+  selectedOperator(op: string) {
+    this.setValues();
+
+    this.operator = op;
+    this.operations += ` ${this.operator} `;
+  }
+
+  setValues() {
+    if (!this.value1) {
+      this.value1 = parseFloat(this.numbers);
+      this.numbers = "";
+    }
+    if (!this.value2) {
+      this.value2 = parseFloat(this.numbers);
+      this.numbers = "";
+    }
+
+    if (this.value1 && this.value2) {
+      this.getResult();
+    }
+  }
+
+  resetOperations() {
+    this.operations = "0";
+    this.value1 = undefined;
+    this.value2 = undefined;
+  }
+
+  getResult() {
+    switch (this.operator) {
+      case '+':
+        this.service.addition(this.value1, this.value2).subscribe({
+          next: result => {
+            this.value1 = result;
+            this.value2 = undefined;
+          }
+        });
+        break;
+      case '-':
+        this.service.subtraction(this.value1, this.value2).subscribe({
+          next: result => {
+            this.value1 = result;
+            this.value2 = undefined;
+          }
+        });
+        break;
+      case '*':
+        this.service.multiplication(this.value1, this.value2).subscribe({
+          next: result => {
+            this.value1 = result;
+            this.value2 = undefined;
+          }
+        });
+        break;
+      case '/':
+        this.service.division(this.value1, this.value2).subscribe({
+          next: result => {
+            this.value1 = result;
+            this.value2 = undefined;
+          }
+        });
+        break;
+      default:
+        break;
+    }
+  }
+
+  calculate() {
+    this.setValues();
+
+    if (this.value1 && this.value2) {
+      setTimeout(() => {
+        this.getResult();
+        this.operations = this.value1;
+      }, 100);
+    }
   }
 
 }
